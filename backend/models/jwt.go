99162d00a00f1cd4c3f1b3e4d2b1fcb58a3dbc8f
Arowolo Abiodun <aarowolom@gmail.com>
2025-02-24 01:13:51 +0100

    Add .gitignore, update JWT secret handling, and clean up build errors

--- backend/models/jwt.go
+++ backend/models/jwt.go
@@ -1,24 +1,23 @@
 package models
 
 import (
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var jwtSecret = []byte("your_secret_key")
-
 // GenerateJWT generates a JWT token for a user
 func GenerateJWT(user User) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"userID": user.ID,
-		"role":   user.Role,
-		"name":   user.Name,
-		"did":    user.DepartmentID,
-		"dept":   user.Department.Name,
-		"exp":    time.Now().Add(time.Hour * 24).Unix(), // Expires in 24 hours
+		"id":   user.ID,
+		"role": user.Role,
+		"name": user.Name,
+		"did":  user.DepartmentID,
+		"dept": user.Department.Name,
+		"exp":  time.Now().Add(time.Hour * 24).Unix(), // Expires in 24 hours
 	})
-
+	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
 	tokenString, err := token.SignedString(jwtSecret)
 	if err != nil {
 		return "", err
