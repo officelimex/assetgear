3c3da51ab0fdeb03b6b2acf4c695e37c36e8db4d
Arowolo Abiodun <aarowolom@gmail.com>
2025-02-24 15:32:37 +0100

    Add CORS configuration, implement OTP generation utility, and create password reset OTP email template

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/officelimex/assetgear/config"
 	"github.com/officelimex/assetgear/database"
@@ -10,20 +12,28 @@ import (
 )
 
 func main() {
-	config.ConnectDB() 
+	config.Init()
 
 	database.Migrate()
 
 	route := gin.Default()
+
+	// Configure CORS middleware
+	route.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{os.Getenv("FRONTEND_URL")},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	endPoints := route.Group("/v1/api")
 	{
-
 		routes.AuthRouter(endPoints)
 		// routes.SetupRouter(endPoints)
 		// routes.SalesCustomerRoute(endPoints)
-
 	}
 
 	log.Println("Server is running on :8080")
-	route.Run(":8080") 
+	route.Run(":8080")
 }
